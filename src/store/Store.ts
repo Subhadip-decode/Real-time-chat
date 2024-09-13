b1b7b14358d96e34c93fda6363aaffa905db7621
Subhadip Nandy <subhadipnandy@Subhadips-MacBook-Air.local>
2024-09-14 01:01:34 +0530

    Adding store classes with basic chat func

---
+++ src/store/Store.ts
@@ -0,0 +1,25 @@
+export type UserId = string
+export interface Chat {
+    userId: string;
+    name: string;
+    message: string;
+    upvotes: UserId[];
+}
+export abstract class Store {
+    constructor(){
+
+    }
+
+    initRoom(roomId: string){
+
+    }
+    getChats(room:string ,limit:number ,offset:number){
+
+    }
+    addChat(room:string, limit:number, offset: number){
+
+    }
+    upvote(room: string, chatId: string){
+
+    }
+}
