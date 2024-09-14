9e9ee73e36633b635a96c2bfe1bfd28ef78babf4
Subhadip Nandy <subhadipnandy@Subhadips-MacBook-Air.local>
2024-09-14 14:12:00 +0530

    added message handling

---
+++ src/messages/IncomingMessages.ts
@@ -0,0 +1,25 @@
+import z from 'zod'
+
+export const InitMessage  = z.object({
+    name: z.string(),
+    userId: z.string(),
+    roomId: z.string(),
+})
+
+export type InitMessageType = z.infer<typeof InitMessage>;
+
+export const UserMessage = z.object({
+    userId: z.string(),
+    roomId: z.string(),
+    message: z.string(),
+})
+
+export type UserMessageType = z.infer<typeof UserMessage>;
+
+export const UpvoteMessage  = z.object({
+    userId: z.string(),
+    roomId: z.string(),
+    chatId: z.string()
+})
+
+export type UpvoteMessageType = z.infer<typeof UpvoteMessage>;
